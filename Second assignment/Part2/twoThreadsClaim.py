959e36eb31fd442a9273c2e48adfb53d70a5ac8b
Luca Rizzo <lucalucasrizzo@gmail.com>
2024-01-07 12:50:05 +0100

    integrated twoThreadsClaims with test and observation

--- Second assignment/Part2/twoThreadsClaim.py
+++ Second assignment/Part2/twoThreadsClaim.py
@@ -49,3 +49,42 @@ def _thread_fun(seq_iter: int, fun, *args, **kwargs):
     for _ in range(seq_iter):
         fun(*args, **kwargs)
 
+def just_wait(n): # NOOP for n/10 seconds
+    time.sleep(n * 0.1)
+    
+def grezzo(n): # CPU intensive
+    for _ in range(2**n):
+        pass
+
+def test(iter, fun, args):
+    n_threads = (1, 2, 4, 8)
+    seq_iter = (16, 8, 4, 2)
+    result_single_thread = bench(n_threads=n_threads[0], seq_iter=seq_iter[0], iter=iter)(fun)(*args)
+    result_two_threads = bench(n_threads=n_threads[1], seq_iter=seq_iter[1], iter=iter)(fun)(*args)
+    result_four_threads = bench(n_threads=n_threads[2], seq_iter=seq_iter[2], iter=iter)(fun)(*args)
+    result_eight_threads = bench(n_threads=n_threads[3], seq_iter=seq_iter[3], iter=iter)(fun)(*args)
+    filename = f"{fun.__name__}-{args}-{n_threads}-{seq_iter}.txt"
+    with open(filename, 'w') as file:
+        file.write(str(result_single_thread) + "\n")
+        file.write(str(result_two_threads) + "\n")
+        file.write(str(result_four_threads) + "\n")
+        file.write(str(result_eight_threads) + "\n")
+        
+        
+
+# Example test with 'just_wait' function
+test(iter=5, fun=just_wait, args=(1,))
+
+# Example test with 'grezzo' function
+test(iter=5, fun=grezzo, args=(12,))
+
+
+# This experiment demonstrates that multithreading in Python is particularly efficient for blocking operations, such as those involving IO,
+# where a thread is waiting for some external event. In such cases, other threads can continue execution, making better use of system resources.
+# For example, in a CRUD web application, where a significant amount of time is spent waiting for IO operations to complete,
+# such as reading all the byte of a request from the socket and writing data to the database, multithreading allows the processing of multiple requests "in parallel",
+# in the sense that, even if the first request is not yet terminated and is waiting for DB writing, a second request can be processed.
+# However, for CPU-intensive operations where each thread competes for CPU time, multithreading may not provide significant efficiency gains.
+# This is due to the Global Interpreter Lock (GIL), which allows only one thread to execute code at a time.
+# So, even though multiple threads are ready, only one of them can execute at a time, and when it is its turn,
+# there is a context-switching overhead before it executes. In such CPU-bound scenarios, single-threaded execution may be more performant.
